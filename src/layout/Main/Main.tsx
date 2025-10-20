93f9386e202abcf0698c5261411e2e3aff0d9daa
appleSimple <wldus8057@gmail.com>
2025-10-20 15:42:52 +0900

    메인이미지 변경

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
-import mainImg from '@/assets/images/05.jpg'
+import mainImg from '@/assets/images/05.webp';
 
 const Main = () => {
   const { greeting } = data;
   return (
     <div>
-      <MainImg src={mainImg} />
+      <MainImg />
       <MainTitle>{greeting.title}</MainTitle>
       <SubTitle>{greeting.eventDetail}</SubTitle>
     </div>
@@ -15,24 +15,28 @@ const Main = () => {
 
 export default Main;
 
-const MainImg = styled.img`
+const MainImg = styled.div`
   border-radius: 200px 200px 0 0;
-  width: 90%;
+  width: 100%;
   max-width: 450px;
-  padding-top: 20px;
+  min-height: 400px;
+  background-image: url(${mainImg});
+  background-size: 600px;
+  background-position: 64% 60%;
+  background-repeat: no-repeat;
 `;
 
 const MainTitle = styled.p`
-  font-family: HSSanTokki20-Regular, serif;
+  font-family: Yangjin, serif;
   font-size: 2rem;
-  color: #2F2120;
+  color: #2f2120;
   line-height: 120%;
   white-space: pre-line;
 `;
 
 const SubTitle = styled.p`
   font-size: 1.1rem;
-  color: #2F2120;
+  color: #2f2120;
   line-height: 140%;
   white-space: pre-line;
 `;
