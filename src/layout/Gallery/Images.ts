92d5f0a807709d8ece366f108fa4a4f977bef71e
JiYeon <73057489+appleSimple@users.noreply.github.com>
2025-12-03 21:29:21 +0900

    Update Images.ts

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -3,6 +3,17 @@ import image02 from '@/assets/images/02.webp';
 import image03 from '@/assets/images/03.webp';
 import image04 from '@/assets/images/04.webp';
 import image05 from '@/assets/images/05.webp';
+import image06 from '@/assets/images/06.webp';
+import image07 from '@/assets/images/07.webp';
+import image08 from '@/assets/images/08.webp';
+import image09 from '@/assets/images/09.webp';
+import image10 from '@/assets/images/10.webp';
+import image11 from '@/assets/images/11.webp';
+import image12 from '@/assets/images/12.webp';
+import image13 from '@/assets/images/13.webp';
+import image14 from '@/assets/images/14.webp';
+import image15 from '@/assets/images/15.webp';
+import image16 from '@/assets/images/16.webp';
 
 const images = [
   /* 이미지 경로를 리스트로 저장 */
@@ -35,6 +46,66 @@ const images = [
     source: image05,
     width: 640,
     height: 960,
+  },
+  {
+    alt: 'image06',
+    source: image06,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image07',
+    source: image07,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image08',
+    source: image08,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image09',
+    source: image09,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image10',
+    source: image10,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image11',
+    source: image11,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image12',
+    source: image12,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image13',
+    source: image13,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image15',
+    source: image15,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image16',
+    source: image16,
+    width: 960,
+    height: 640,
   },
 ];
 
